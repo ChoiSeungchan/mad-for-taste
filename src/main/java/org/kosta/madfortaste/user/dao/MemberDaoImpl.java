bd2648c3e1452bd3f16fd7afe184fe9e097b5cc6
Daniel <hs9923@naver.com>
2015-06-07 01:53:00 +0900

    Junit 테스트환경 셋팅(annotation-driven) Junit Version UP (4.7 -> 4.12)

--- src/main/java/org/kosta/madfortaste/user/dao/MemberDaoImpl.java
+++ src/main/java/org/kosta/madfortaste/user/dao/MemberDaoImpl.java
@@ -1,5 +1,13 @@
 package org.kosta.madfortaste.user.dao;
 
-public class MemberDaoImpl implements MemberDao {
+import javax.annotation.Resource;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDaoImpl implements MemberDao {
+	
+	@Resource
+	private SqlSessionTemplate sqlSessionTemplate;
 }
--- src/test/java/org/kosta/madfortaste/user/dao/TestMemberDao.java
+++ src/test/java/org/kosta/madfortaste/user/dao/TestMemberDao.java
@@ -1,14 +1,27 @@
 package org.kosta.madfortaste.user.dao;
+import static org.junit.Assert.assertNotNull;
 
-import static org.junit.Assert.*;
+import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/root-context.xml")
 public class TestMemberDao {
+	
+	@Resource
+	MemberDao memberDao;
+	
+	@Test
+	public void setUp() {
+		assertNotNull(memberDao);
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
